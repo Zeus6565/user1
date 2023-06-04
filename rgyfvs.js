65be14a0c26eeb690e59b4cc47f89a912d259330
Zeus6565 <130297181+Zeus6565@users.noreply.github.com>
2023-06-04 14:55:49 +0800

    Create rgyfvs.js

---
+++ rgyfvs.js
@@ -0,0 +1,5 @@
+    // Function to show the content and hide the login form
+    function showContent() {
+      document.getElementById("content").style.display = "block";
+      document.getElementById("loginForm").style.display = "none";
+    }
