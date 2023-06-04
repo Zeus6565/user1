703202ac28fac29fd0a1391bdab3dfca6817e2d3
Zeus6565 <130297181+Zeus6565@users.noreply.github.com>
2023-06-04 15:00:56 +0800

    rgyfvs.js

--- rgyfvs.js
+++ rgyfvs.js
@@ -1,5 +1,14 @@
-    // Function to show the content and hide the login form
-    function showContent() {
-      document.getElementById("content").style.display = "block";
-      document.getElementById("loginForm").style.display = "none";
+    // Function to handle form submission
+    function submitForm() {
+      var passwordInput = document.getElementById("password").value;
+      var hashedInput = sha256(passwordInput);
+      var passwordHash = "7a2ec40ff8a1247c532309355f798a779e00acff579c63eec3636ffb2902c1ac";
+
+      if (hashedInput === passwordHash) {
+        setCookie("password", hashedInput, 30); // Set the cookie for 30 seconds
+        showContent();
+      } else {
+        alert("Invalid password. Please try again.");
+      }
+      return false;
     }
